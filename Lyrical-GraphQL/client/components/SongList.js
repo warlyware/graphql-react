4efb911cef081ad7412c2e043e4be605f30a4bb2
Dan Ward <daniel.ward@forrent.com>
2018-04-03 16:59:40 -0400

    Add React app boilerplate, SongList component

---
+++ Lyrical-GraphQL/client/components/SongList.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class SongList extends Component {
+  render() {
+    return(
+      <div>
+        Song List
+      </div>
+    )
+  }
+}
