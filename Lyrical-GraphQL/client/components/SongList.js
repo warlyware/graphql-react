513c43e8d3d5373b66b421bede973fdd30e2db2b
Dan Ward <daniel.ward@forrent.com>
2018-04-03 17:19:01 -0400

    Add gql query to SongList component, display data

--- Lyrical-GraphQL/client/components/SongList.js
+++ Lyrical-GraphQL/client/components/SongList.js
@@ -1,11 +1,35 @@
 import React, { Component } from 'react';
+import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
 
-export default class SongList extends Component {
+class SongList extends Component {
+  renderSongs() {
+    return this.props.data.songs.map((song) => {
+      return (
+        <li className="collection-item" key={song.id}>
+          {song.title}
+        </li>
+      );
+    });
+  }
+
   render() {
+    if (this.props.data.loading) { return <div>Loading...</div> }
     return(
-      <div>
-        Song List
+      <div className="collection">
+        {this.renderSongs()}
       </div>
     )
   }
 }
+
+const query = gql`
+  {
+    songs {
+      title
+      id
+    }
+  }
+`;
+
+export default graphql(query)(SongList);
