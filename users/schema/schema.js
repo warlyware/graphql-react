a156a436200c46fe3bcb77465ec56a420e5c17b3
Dan Ward <daniel.ward@forrent.com>
2018-04-03 12:26:43 -0400

    Add first schema, first query

---
+++ users/schema/schema.js
@@ -0,0 +1,39 @@
+const graphql = require('graphql');
+const _ = require('lodash');
+const {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLInt,
+  GraphQLSchema
+} = graphql;
+
+const users = [
+  {id: '23', firstName: 'Bob', age: 45},
+  {id: '47', firstName: 'Sam', age: 19}
+]
+
+const UserType = new GraphQLObjectType({
+  name: 'User',
+  fields: {
+    id: { type: GraphQLString },
+    firstName: { type: GraphQLString },
+    age: { type: GraphQLInt }
+  }
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    user: {
+      args: { id: { type: GraphQLString } },
+      type: UserType,
+      resolve(parentValue, args) {
+        return _.find(users, { id: args.id });
+      }
+    }
+  }
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery
+});
